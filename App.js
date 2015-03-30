915b8e48d24ce3fc5ca1d266e27d08f9c054beea
simon <sitiu@deloitte.com>
2015-03-30 15:28:48 -0700

    initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    launch: function() {
+        //Write app code here
+        console.log('hello world');
+
+        //API Docs: https://help.rallydev.com/apps/2.0/doc/
+    }
+});
